dcc9e0149773bfada0bf5dd6967bebb595cc0a02
chlyzbll <24-00418@g.batstate-u.edu.ph>
2025-11-21 01:06:58 +0800

    update

--- src/main/java/tournament/Tournament.java
+++ src/main/java/tournament/Tournament.java
@@ -45,7 +45,7 @@ public abstract class Tournament {
         for (int i = 0; i < participants.size(); i += 2) {
             if (i + 1 < participants.size()) {
                 System.out.println("|-- " + participants.get(i).getDisplayName());
-                System.out.println("|      \");
+                System.out.println("|      \\");
                 System.out.println("|       > TBD Winner");
                 System.out.println("|      /");
                 System.out.println("|-- " + participants.get(i + 1).getDisplayName() + "\n");
@@ -75,22 +75,26 @@ public abstract class Tournament {
                 if (i + 1 < round.size()) {
                     Participant p1 = round.get(i);
                     Participant p2 = round.get(i + 1);
+
                     Participant winner = rng.nextBoolean() ? p1 : p2;
 
                     System.out.println("|-- " + p1.getDisplayName());
-                    System.out.println("|      \");
+                    System.out.println("|      \\");
                     System.out.println("|       > Winner: " + winner.getDisplayName());
                     System.out.println("|      /");
                     System.out.println("|-- " + p2.getDisplayName() + "\n");
 
                     nextRound.add(winner);
+
                     Participant loser = (winner == p1) ? p2 : p1;
                     eliminatedRound.put(loser, roundNum);
+
                 } else {
                     System.out.println("|-- " + round.get(i).getDisplayName() + " advances automatically (bye)\n");
                     nextRound.add(round.get(i));
                 }
             }
+
             round = nextRound;
             roundNum++;
         }
@@ -100,17 +104,21 @@ public abstract class Tournament {
 
         List<Participant> ranking = new ArrayList<>(participants);
         ranking.remove(champion);
+
         int finalRoundNum = roundNum;
+
         ranking.sort((a, b) -> {
             int r1 = eliminatedRound.getOrDefault(a, finalRoundNum);
             int r2 = eliminatedRound.getOrDefault(b, finalRoundNum);
             return Integer.compare(r2, r1);
         });
+
         ranking.add(0, champion);
 
         System.out.println("\n=== FINAL RANKINGS ===");
-        for (int i = 0; i < ranking.size(); i++)
+        for (int i = 0; i < ranking.size(); i++) {
             System.out.printf("%d. %s%n", i + 1, ranking.get(i).getDisplayName());
+        }
     }
 
     public abstract Participant createParticipantFromInput(Scanner sc);
